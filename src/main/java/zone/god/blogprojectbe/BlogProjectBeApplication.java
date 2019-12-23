d6af196f2985d186eee08cc881f05526aeec1b78
zonesama <trannhatthanh31@gmail.com>
2019-12-23 16:11:10 +0700

    First Commit

---
+++ src/main/java/zone/god/blogprojectbe/BlogProjectBeApplication.java
@@ -0,0 +1,13 @@
+package zone.god.blogprojectbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogProjectBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlogProjectBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/zone/god/blogprojectbe/BlogProjectBeApplicationTests.java
@@ -0,0 +1,13 @@
+package zone.god.blogprojectbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogProjectBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
